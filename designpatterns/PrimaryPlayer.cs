8d16529f1bcc45a9903c34a7bdd803722f30f5df
rds80 <rippleshah@msn.com>
2018-07-05 23:25:44 -0400

    Examples of design patterns

---
+++ designpatterns/PrimaryPlayer.cs
@@ -0,0 +1,28 @@
+﻿namespace designpatterns
+{
+    public sealed class PrimaryPlayer
+    {
+        private static readonly PrimaryPlayer _instance;
+        public string Name { get; set; }
+        public int Level { get; set; }
+
+        private PrimaryPlayer() { }
+
+        static PrimaryPlayer()
+        {
+            _instance = new PrimaryPlayer()
+            {
+                Name = "Raptor",
+                Level = 1
+            };
+        }
+
+        public static PrimaryPlayer Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
---
+++ designpatterns/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace designpatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimaryPlayer player = PrimaryPlayer.Instance;
+            WriteLine($"{player.Name} - lvl {player.Level}");
+            ReadKey();
+        }
+    }
+}
